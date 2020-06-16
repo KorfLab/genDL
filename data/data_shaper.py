8e9df0c45407f301953752a0d19f5073b534bf5d
xperthunter <kjfraga@ucdavis.ed>
2020-06-16 15:54:29 -0700

    updates to data_shaper.py. genlearning.py is volatile.

--- data/data_shaper.py
+++ data/data_shaper.py
@@ -5,27 +5,34 @@ import sys
 import os
 import gzip
 import pickle
+import random
 
 import numpy as np
 from tensorflow import keras
 
-parser = argparse.ArgumentParser(description=''.join(('Shape splice-site data,',
-	'into one-hot encoding matrices')))
+parser = argparse.ArgumentParser(description=''.join(('Shape splice-site data',
+	' into one-hot encoding matrices')))
 parser.add_argument('--true', required=True, type=str,
     metavar='<str>', help='true donor/acceptor data')
 parser.add_argument('--fake', required=True, type=str,
 	metavar='<str>', help='fake donor/acceptor data')
 parser.add_argument('--type', required=True, type=str,
 	metavar='<str>', help='acceptor or donor examples')
-parser.add_argument('--size', required=False, type=int, default=10000,
-	metavar='<str>', help='number of examples to process')
+parser.add_argument('--level', required=True, type=str,
+	metavar='<str>', help='hi, lo, or hilo')
+parser.add_argument('--number', required=False, type=int, default=1000,
+	metavar='<int>', help='number of examples desired. -1 if you want all data')
+parser.add_argument('--window', required=False, type=int, default=42,
+	metavar='<int>', help='size of sequences')
 
 arg = parser.parse_args()
 
 assert(os.path.isdir('/'.join((os.getcwd(), 'one_hot'))))
 assert(arg.type == 'acceptor' or arg.type == 'donor')
+assert(arg.level == 'hi' or arg.level == 'lo' or arg.level == 'hilo')
 
 dna_int = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
+int_dna = {0: 'A', 1: 'C', 2: 'G', 3: 'G'}
 
 def read_data(filename):
 	fp = None
@@ -37,6 +44,7 @@ def read_data(filename):
 
 	for line in fp.readlines():
 		line = line.rstrip()
+		assert(len(line) == arg.window)
 		yield(line)
 		
 	fp.close()
@@ -45,29 +53,42 @@ def to_integer(seq):
 	intlist = list()
 	for nt in seq:
 		intlist.append(dna_int[nt])
-	return np.array(intlist, dtype=np.float64)
+	return np.array(intlist, dtype=np.uint8)
 
-def pickling(type, bool, size, data):
-	filepath = f"{os.getcwd()}/one_hot/{type}.{bool}.{size}.pickle"
+def seqs_list(filename):
+	seqs = list()
+	for seq in read_data(filename):
+		seqs.append(to_integer(seq))
+	
+	return seqs
+
+def pickling(type, bool, size, level, data):
+	if size == -1:
+		size = 'all'
+	filepath = f"{os.getcwd()}/one_hot/{type}.{bool}.{level}.{size}.pickle"
 	print(filepath)
 	pickle_out = open(filepath, "wb")
 	pickle.dump(data, pickle_out)
 	pickle_out.close()
 	
 def one_hotter(file, size):
-	counter = 0
-	array = np.zeros((size, 42, 4), dtype=np.float64)
-	for seq in read_data(file):
-		seqdata = to_integer(seq)
-		encoded = keras.utils.to_categorical(seqdata)
-		array[counter,:,:] = encoded
-		counter+=1
-		if counter >= arg.size:
-			break
+	seqs = seqs_list(file)
+	random.shuffle(seqs)
+	if size == -1:
+		arg.number = len(seqs)
+
+	array = np.zeros((arg.number, arg.window, 4), dtype=np.float64)
+	for i, seq in enumerate(seqs[:arg.number]):
+		encoded = keras.utils.to_categorical(seq, 
+											 num_classes=4,
+											 dtype=np.float64)
+		
+		array[i,:,:] = encoded
+	
 	return array
 	
-true = one_hotter(arg.true, arg.size)
-fake = one_hotter(arg.fake, arg.size)
+true = one_hotter(arg.true, arg.number)
+fake = one_hotter(arg.fake, arg.number)
 
-pickling(arg.type, 'true', arg.size, true)
-pickling(arg.type, 'fake', arg.size, fake)
+pickling(arg.type, 'true', arg.number, arg.level, true)
+pickling(arg.type, 'fake', arg.number, arg.level, fake)
--- train/genlearning.py
+++ train/genlearning.py
@@ -40,16 +40,29 @@ print(truelabel.shape, fakelabel.shape)
 
 model = keras.Sequential([
 	keras.layers.Flatten(input_shape=(42,4)),
-	keras.layers.Dense(82, activation='relu'),
-	keras.layers.Dense(42, activation='relu'),
+	keras.layers.Dense(84, activation='elu', 
+		kernel_regularizer=tf.keras.regularizers.l2(0.001)),
+	keras.layers.Dropout(0.50),
+	keras.layers.Dense(42, activation='elu',
+		kernel_regularizer=tf.keras.regularizers.l2(0.001)),
+	keras.layers.Dropout(0.50),
+	keras.layers.Dense(21, activation='elu',
+		kernel_regularizer=tf.keras.regularizers.l2(0.001)),
+	keras.layers.Dropout(0.25),
+	keras.layers.Dense(10, activation='elu',
+		kernel_regularizer=tf.keras.regularizers.l2(0.0001)),
+#	keras.layers.Dropout(0.25),
+	keras.layers.Dense(5, activation='elu',
+		kernel_regularizer=tf.keras.regularizers.l2(0.0001)),
+#	keras.layers.Dropout(0.25),
 	keras.layers.Dense(2, activation='softmax')
 ])
 
-model.compile(optimizer='adam',
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-5),
 	loss=tf.keras.losses.CategoricalCrossentropy(),
-	metrics=['accuracy'])
+	metrics=['accuracy',tf.keras.metrics.Precision(),tf.keras.metrics.Recall()])
 	
-model.fit(seqs, labels, epochs=10, batch_size=1, validation_split=0.20)
+model.fit(seqs, labels, epochs=30, batch_size=1, validation_split=0.20)
 """
 test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
 print('\nTest accuracy:', test_acc)
