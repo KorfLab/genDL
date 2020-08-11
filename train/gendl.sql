aa2362a68e768b717b0414ffb8018b44c65e4895
xperthunter <kjfraga@ucdavis.edu>
2020-08-10 17:47:51 -0700

    starting to make schema for saving models

---
+++ train/gendl.sql
@@ -0,0 +1,29 @@
+CREATE TABLE models (
+	model_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	depth INTEGER,
+	sizes TEXT,
+	lr REAL,
+	batch INTEGER,
+	epoch INTEGER,
+	reg TEXT,
+	dropout TEXT,
+);
+
+CREATE TABLE metrics (
+	model_id INTEGER,
+	tpr REAL,
+	tnr REAL,
+	ppv REAL,
+	npv REAL,
+	fsc REAL,
+	FOREIGN KEY ([model_id]) REFERENCES "models" ([model_id]) ON DELETE ACTION ON UPDATE NO ACTION
+);
+
+CREATE TABLE data (
+	model_id INTEGER,
+	type TEXT,
+	level TEXT,
+	number INTEGER,
+	name TEXT,
+	FOREIGN KEY ([model_id]) REFERENCES "models" ([model_id]) ON DELETE ACTION ON UPDATE NO ACTION
+);
