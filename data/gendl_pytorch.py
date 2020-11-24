9c79bc8a09cc568d8b6068f39d05b8a88344e259
Amalia <58994466+akaresh@users.noreply.github.com>
2020-11-23 17:47:19 -0800

    tensorboard

--- data/gendl_pytorch.py
+++ data/gendl_pytorch.py
@@ -9,20 +9,24 @@ import gzip
 import random
 import pickle
 from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils
+#from torchvision import transforms, utils
 from sklearn.model_selection import train_test_split
 
 from dynamic_model import DynamicNet
 
+#from torch.utils.tensorboard import SummaryWriter
+#import torchvision
+
+
 parser = argparse.ArgumentParser(description='Pytorch')
 parser.add_argument('--true', required=True, type=str,
-    metavar='<path>', help='path to true pickled file')
+	metavar='<path>', help='path to true pickled file')
 parser.add_argument('--fake', required=True, type=str,
-    metavar='<path>', help='path to true pickled file')
+	metavar='<path>', help='path to true pickled file')
 parser.add_argument('--epoch', required=False, type=int, default = 10,
-    metavar='<int>', help='number of epochs')
+	metavar='<int>', help='number of epochs')
 parser.add_argument('--batch', required=False, type=int, default = 1,
-    metavar='<int>', help='batch size')
+	metavar='<int>', help='batch size')
 
 arg = parser.parse_args()
 
@@ -49,97 +53,101 @@ val_seqs = torch.utils.data.DataLoader(seq_val, batch_size = 1, shuffle = False)
 val_labels = torch.utils.data.DataLoader(label_val, batch_size = 1, shuffle = False)
 
 
-class Net(nn.Module):
-    def __init__(self):
-        super().__init__()
-        #self.conv1 = nn.Conv1d(in_channels = 42, out_channels= 84 , kernel_size = 2)
-        #self.maxpool = nn.MaxPool2d(42, stride=2)
-        self.fc1 = nn.Linear(in_features=168, out_features=84)
-        self.fc2 = nn.Linear(in_features=84, out_features=42)
-        #self.conv2 = nn.Conv2d(in_channels = 42, out_channels = 42, kernel_size = 3)
-        self.fc3 = nn.Linear(in_features=42, out_features=24)
-        self.fc4 = nn.Linear(in_features=24, out_features=12)
-        self.out = nn.Linear(in_features=12, out_features=1) ###
-
-
-    def forward(self, t):
-        #t = F.relu(self.conv1(t))
-
-        #t = t.flatten(start_dim=1)
-        #t = F.max_pool2d(t, kernel_size=2, stride=2)
-
-        #t = F.relu(self.conv2(t))
-        #t = F.max_pool2d(t, kernel_size=2, stride=2)
-
-        t = t.flatten(start_dim=1)
-        t = F.elu(self.fc1(t))
-        t = F.dropout(t, training=self.training)
-        t = F.elu(self.fc2(t))
-        t = F.dropout(t, training=self.training)
-        #t = F.elu(self.conv2())
-        t = torch.tanh(self.fc3(t))
-        t = F.dropout(t, training=self.training)
-        t = torch.tanh(self.fc4(t))
-        t = F.dropout(t, training=self.training)
-        t = torch.sigmoid(self.out(t))
-        #t = self.out(t)
-
-        return (t)
-
-model9 = DynamicNet(((len(seqs[0]))*4), [21, 10], [nn.ELU, nn.ELU], [0.1, 0.1])
+model9 = DynamicNet(len(seqs[0])*4, [21, 10], [nn.ReLU, nn.ReLU])
 nets = []
 nets.append(model9)
+
+#tensorboard to draw the model
+#tb = SummaryWriter()
 
 #training the network
 #print(len(train_seqs))
 #print(len(train_labels))
+
+def get_num_correct(preds, labels):
+	return preds.argmax(dim=1).eq(labels).sum().item()
+
 accuracy = []
 for net in nets:
-    #defining a loss function and optimizer
-    criterion = nn.BCELoss()
-    #optimizer = optim.SGD(net.parameters(), lr=1e-4, momentum=0.9, weight_decay = 1e-4)
-    optimizer = optim.Adam(net.parameters(), lr=1e-4, weight_decay = 1e-4)
-    ##try a different optimizer
-    for epoch in range(arg.epoch):  # loop over the dataset multiple times
-        running_loss = 0.0
-        for i, data in enumerate(zip(train_seqs, train_labels), 0):
-            seq, label = data
-            # zero the parameter gradients
-            optimizer.zero_grad()
-            seq = seq.float()
-            label = label.float()
-            outputs = net(seq)
-            #print(outputs, label)
-            loss = criterion(outputs, label)
-            loss.backward()
-            optimizer.step()
+	#defining a loss function and optimizer
+	criterion = nn.BCELoss()
+	#optimizer = optim.SGD(net.parameters(), lr=1e-4, momentum=0.9, weight_decay = 1e-4)
+	optimizer = optim.Adam(net.parameters(), lr=1e-4, weight_decay = 1e-4)
+	##try a different optimizer
+	for epoch in range(arg.epoch):  # loop over the dataset multiple times
+		total_correct = 0
+		total_loss = 0
+		running_loss = 0.0
+		for i, data in enumerate(zip(train_seqs, train_labels), 0):
+			seq, label = data
+			# zero the parameter gradients
+			optimizer.zero_grad()
+			seq = seq.float()
+			label = label.float()
+			label = label.unsqueeze(1)
+			outputs = net(seq)
+			#print(outputs, label)
+			#print(outputs, len(outputs))
+			#print(label)
+			#sys.exit()
+			loss = criterion(outputs, label)
 
-            # print statistics
-            running_loss += loss.item()
-            if i % 100 == 0:    # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' % (epoch + 1, i, running_loss / 100))
-                running_loss = 0.0
+			#writing to scalar
+			#tb.add_scalar("Loss/train", loss, epoch)
+			###
+			loss.backward()
+			optimizer.step()
 
-    print('Finished Training')
+			##accuracy and loss in training set
+			total_correct += get_num_correct(outputs, label)
+			total_loss +=loss.item()
 
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for test_seq, test_label in zip(val_seqs, val_labels):
-            #test_seq = test_seq.clone().detach()
-            #test_seq = torch.tensor(test_seq, dtype=torch.float32)
-            test_seq = test_seq.float()
-            outputs = net(test_seq)
-            predicted = torch.round(outputs)
-            #torch.mac() = 0 always
-            total += test_label.size(0)
-            if predicted == test_label:
-                correct += 1
-    accuracy.append((correct/total))
+			# print statistics
+			running_loss += loss.item()
+			if i % 100 == 0:    # print every 2000 mini-batches
+				print('[%d, %5d] loss: %.3f' % (epoch + 1, i, running_loss / 100))
+				###recording training loss form each epoch into the writer
+				running_loss = 0.0
+		#print(total_correct/len(seq_train))
+		#sys.exit()
+		#tb.add_scalar("Loss", total_loss, epoch)
+		#tb.add_scalar("Correct", total_correct, epoch)
+		#tb.add_scalar("Accuracy", total_correct/len(seq_train), epoch)
+			#changing the batch size
+
+		for name, weight in model9.named_parameters():
+			#tb.add_histogram(name, weight, epoch)
+			#tb.add_histogram(f'{name}.grad', weight.grad, epoch)
+	#tb.flush()
+	print('Finished Training')
+
+	correct = 0
+	total = 0
+	with torch.no_grad():
+		for test_seq, test_label in zip(val_seqs, val_labels):
+			#test_seq = test_seq.clone().detach()
+			#test_seq = torch.tensor(test_seq, dtype=torch.float32)
+			test_seq = test_seq.float()
+			outputs = net(test_seq)
+			predicted = torch.round(outputs)
+			#torch.max() = 0 always
+			total += test_label.size(0)
+			if predicted == test_label:
+				correct += 1
+
+			#recording loss and accuracy from the test run into the writer
+	accuracy.append((correct/total))
+
+#tb.close()
+
 
 for i in range(len(accuracy)):
-    print(f'Accuracy of model {i+1}: {accuracy[i]:.4f}')
+	print(f'Accuracy of model {i+1}: {accuracy[i]:.4f}')
 
 ###graphic of the network (weights and bias for each node)
 
 #print(f'Accuracy of the network on the test sequences: {(correct/total):.4f}')
+
+###checking on the train - round works, while max does not
+###how the network is being initialized
+###drop, regular, learning rate
