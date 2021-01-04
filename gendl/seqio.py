ecef5bed4bb6cab5e7e8c1dd2635bebb63ee1ba8
Ian Korf <ifkorf@ucdavis.edu>
2021-01-04 12:32:29 -0800

    base libs and tests

---
+++ gendl/seqio.py
@@ -0,0 +1,53 @@
+import gzip
+import random
+import sys
+
+def read_fasta(filename):
+	name = None
+	seqs = []
+	
+	fp = None
+	if filename == '-':
+		fp = sys.stdin
+	elif filename.endswith('.gz'):
+		fp = gzip.open(filename, 'rt')
+	else:
+		fp = open(filename)
+
+	for line in fp.readlines():
+		line = line.rstrip()
+		if line.startswith('>'):
+			if len(seqs) > 0:
+				seq = ''.join(seqs)
+				yield(name, seq)
+				name = line[1:]
+				seqs = []
+			else:
+				name = line[1:]
+		else:
+			seqs.append(line)
+	yield(name, ''.join(seqs))
+	fp.close()
+
+def read_raw(filename):
+	fp = None
+	if filename == '-':
+		fp = sys.stdin
+	elif filename.endswith('.gz'):
+		fp = gzip.open(filename, 'rt')
+	else:
+		fp = open(filename)
+		
+	for line in fp.readlines():
+		line = line.rstrip()
+		yield(line)
+	
+	fp.close()
+
+def random_dna(length):
+	nts = 'ACGT'
+	seq = []
+	for i in range(length):
+		seq.append(random.choice(nts))
+	return ''.join(seq)
+
