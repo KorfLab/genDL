b6951942d1829be0bd71993aa91e8d88876ba3f4
Ian Korf <ifkorf@ucdavis.edu>
2021-01-04 16:50:34 -0800

    stub

--- pwm/strawman/strawman.py
+++ pwm/strawman/strawman.py
@@ -1,3 +1,13 @@
 
-import gendl
+import argparse
+
+parser = argparse.ArgumentParser(
+	description='Evaluate the performance of simple PWM methods')
+parser.add_argument('--file1', required=True, type=str,
+	metavar='<file>', help='fasta file')
+parser.add_argument('--file2', required=True, type=str,
+	metavar='<file>', help='fasta file')
+arg = parser.parse_args()
+
+
 
