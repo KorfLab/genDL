93f6826ab94ac273215e68100fcc42ec97812a10
Katie Chiu <katiechiu@Katies-MacBook-Pro.local>
2022-02-10 12:10:04 -0800

    backup progress

--- nn/katherine/cnn/convolution.py
+++ nn/katherine/cnn/convolution.py
@@ -13,6 +13,7 @@ image = np.array([[0,1,2,3,4,5],
 # Filter size is chosen to be 3x3
 # Filter values are what will be optimized
 filter = np.random.rand(3,3)
+print(filter)
 
 m = image.shape[0]  # rows
 n = image.shape[1]  # columns
@@ -20,6 +21,8 @@ n = image.shape[1]  # columns
 for i in range(m-2):
   for j in range(n-2):
     submatrix = image[i:i+3, j:j+3]
-    prod = np.dot(submatrix, filter)
-    print(prod)
+    #prod = np.multiply(submatrix, filter).sum().sum()
+    #print(prod)
+    print(submatrix)
+    print(np.amax(submatrix))
     sys.exit()
