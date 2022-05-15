e5a030684b948da91577751226051517d86bb99a
iankorf <ifkorf@gmail.com>
2022-05-15 07:39:25 -0700

    results

---
+++ nn/3lp-demo/run2.pl
@@ -0,0 +1,14 @@
+#python3 mlp.py --file1 ../../data42/at.don.fa.gz --file0 ../../data42/at.n2don.fa.gz --layers 168 1
+
+my @gen = qw(at ce dm);
+my @sig = qw(don acc);
+my @neg = qw(n1 n2 n3);
+
+foreach my $gen (@gen) {
+	foreach my $sig (@sig) {
+		foreach my $neg (@neg) {
+			print STDERR "$gen $sig $neg\n";
+			system("python3 3lp.py --file1 ../../data42/$gen.$sig.fa.gz --file0 ../../data42/$gen.$neg$sig.fa.gz --layer2 84 --layer3 42");
+		}
+	}
+}
