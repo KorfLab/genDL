950bf24690e2bb1f0a3d39c2e43f467344f77520
Ian Korf <ifkorf@ucdavis.edu>
2022-05-15 10:02:42 -0700

    lots

--- nn/mlp-demo/run.pl
+++ nn/mlp-demo/run.pl
@@ -1,4 +1,5 @@
-#python3 mlp.py --file1 ../../data42/at.don.fa.gz --file0 ../../data42/at.n2don.fa.gz --layers 168 1
+use strict;
+use warnings;
 
 my @gen = qw(at ce dm);
 my @sig = qw(don acc);
@@ -8,7 +9,7 @@ foreach my $gen (@gen) {
 	foreach my $sig (@sig) {
 		foreach my $neg (@neg) {
 			print STDERR "$gen $sig $neg\n";
-			system("python3 mlp.py --file1 ../../data42/$gen.$sig.fa.gz --file0 ../../data42/$gen.$neg$sig.fa.gz --layers 168 1");
+			system("python3 mlp.py --file1 ../../data42/$gen.$sig.fa.gz --file0 ../../data42/$gen.$neg$sig.fa.gz --layers 168 84 42 1");
 		}
 	}
 }
